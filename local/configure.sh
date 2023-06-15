522c3c20ce45eb4f9910bfcc50e03128c8de40d8
Davide De Tommaso <dtmdvd@gmail.com>
2023-06-15 19:09:02 +0200

    Add home for local user

--- local/configure.sh
+++ local/configure.sh
@@ -23,40 +23,50 @@
 # SOFTWARE.
 
 #!/bin/bash
+
 set -e
 
 CURRENT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
 
 IFS=',' read -r -a groups_array <<< "$USER_GROUPS"
 IFS=',' read -r -a ids_array <<< "$GROUP_IDS"
 
 for i in "${!groups_array[@]}"; do
     printf "%s\t%s\t%s\n" "$i" ${groups_array[$i]} ${ids_array[$i]}
-    if [ $(getent group ${groups_array[$i]}) ]; then groupmod -o ${groups_array[$i]} --gid ${ids_array[$i]}; else groupadd ${groups_array[$i]} --gid ${ids_array[$i]}; fi
+    if getent group "${groups_array[$i]}" >/dev/null; then
+        groupmod -o "${groups_array[$i]}" --gid "${ids_array[$i]}"
+    else
+        groupadd "${groups_array[$i]}" --gid "${ids_array[$i]}"
+    fi
 done
 
-if [ -z "$(getent passwd "${USER_UID}" | cut -d: -f1)" ]; then \
-  if [ $(getent passwd ${USERNAME}) ]; then \
-      echo "User exists" && usermod -u ${USER_UID} ${USERNAME} && groupmod -g ${USER_GID} ${USERNAME} && passwd -d ${USERNAME}; \
-  else \
-      echo "User not found" && useradd ${USERNAME} && passwd -d ${USERNAME} && usermod -u ${USER_UID} ${USERNAME} && groupmod -g ${USER_GID} ${USERNAME}; \
-  fi; \
-else \
-    echo "UID exists" && old_uname=$(getent passwd "${USER_UID}" | cut -d: -f1) && usermod -l ${USERNAME} ${old_uname} &&  groupmod -n ${USERNAME} ${old_uname}; \
-fi;
-
-usermod -ms /bin/bash -d /home/${USERNAME} ${USERNAME}
-#mkdir -p /home/$USERNAME
-#cp /etc/skel/.bashrc /home/$USERNAME
-#cp /etc/skel/.profile /home/$USERNAME
-#cp /etc/skel/.bash_logout /home/$USERNAME
-chown -R $USER_UID:$USER_GID /home/$USERNAME
-
-if [[ " ${groups_array[*]} " =~ " sudo " ]]; then
-    echo $USERNAME ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/$USERNAME
-    chmod 0440 /etc/sudoers.d/$USERNAME
-    usermod -a -G root "$(getent passwd ${USER_UID} | cut -d: -f1)"
+if ! getent passwd "$USER_UID" >/dev/null; then
+  if getent passwd "$USERNAME" >/dev/null; then
+      echo "User exists"
+      usermod -u "$USER_UID" "$USERNAME"
+      groupmod -g "$USER_GID" "$USERNAME"
+      passwd -d "$USERNAME"
+  else
+      echo "User not found"
+      useradd -m -d /home/"$USERNAME" "$USERNAME"
+      passwd -d "$USERNAME"
+      usermod -u "$USER_UID" "$USERNAME"
+      groupmod -g "$USER_GID" "$USERNAME"
+  fi
+else
+    echo "UID exists"
+    old_uname=$(getent passwd "$USER_UID" | cut -d: -f1)
+    usermod -l "$USERNAME" "$old_uname"
+    groupmod -n "$USERNAME" "$old_uname"
 fi
 
-usermod -a -G $GROUP_IDS "$(getent passwd ${USER_UID} | cut -d: -f1)"
+usermod -ms /bin/bash -d "/home/$USERNAME" "$USERNAME"
+chown -R "$USER_UID:$USER_GID" "/home/$USERNAME"
+
+if [[ " ${groups_array[*]} " =~ " sudo " ]]; then
+    echo "$USERNAME ALL=(root) NOPASSWD:ALL" > "/etc/sudoers.d/$USERNAME"
+    chmod 0440 "/etc/sudoers.d/$USERNAME"
+    usermod -a -G root "$(getent passwd "$USER_UID" | cut -d: -f1)"
+fi
+
+usermod -a -G "$GROUP_IDS" "$(getent passwd "$USER_UID" | cut -d: -f1)"
