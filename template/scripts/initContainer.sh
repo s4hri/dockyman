e0666d977b073455473f16296c27500c583f8cd9
Davide De Tommaso <dtmdvd@gmail.com>
2023-08-13 14:20:21 +0200

    refinements

--- template/scripts/initContainer.sh
+++ template/scripts/initContainer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "DOCKYMAN -> Running inizialization script (docker container)"
-set -a
-source /var/dockyman/scripts/env_vars
-set +a
+
+# some other commands ...
+
 exec "$@"
--- template/scripts/logLocalInfo.sh
+++ template/scripts/logLocalInfo.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
 cat /etc/os-release
-echo ""
 date
-echo ""
 lsb_release -a
-echo ""
 aplay -l
-echo ""
 free
-echo ""
 lshw
-echo ""
 docker version
-echo ""
 docker compose version
